b9634ad1d2fb72c1b02c42aaff8ac75d3975aeec
jim <757109436@qq.com>
2015-12-15 09:13:32 +0800

    。

---
+++ footer.php
@@ -0,0 +1,15 @@
+      <hr>
+
+      <footer>
+        <p>&copy; 2015 BOTANmed, Inc.</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>BOTANmed 泊天</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/css.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+            <img src="images/logo.png" width="235" height="49">
+          </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">首页</a></li>
+            <li class="dropdown submenu">
+              <a href="#product" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">产品中心<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">新品推荐</a></li>
+                <li><a href="#">天然植萃系列</a></li>
+                <li><a href="#">花果美颜系列</a></li>
+                <li><a href="#">药妆功效系列</a></li>
+              </ul>
+            </li>
+            <li  class="dropdown submenu">
+              <a href="#some" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">天然魔力<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">原料介绍</a></li>
+              </ul>
+            </li>
+            <li><a href="#video">视频信息</a></li>
+            <li><a href="#about">关于泊天</a></li>
+            <li><a href="#joinin">招商加盟</a></li>
+            <li><a href="#contact">联系我们</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <div class="form-group">
+              <input placeholder="请输入搜索内容" type="text" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">搜索</button>
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
